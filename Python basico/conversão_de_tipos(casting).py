2ae398404b0cd84a7034e1bfd0017ecac08c3bcd
Kauan Cruz <kauancruzdasilva2@gmail.com>
2026-09-21 13:27:21 -0300

    Aula 6 - Tipos de dados primitivos

---
+++ Python basico/conversão_de_tipos(casting).py
@@ -0,0 +1,14 @@
+"""valor1 = input ("Digite um numero: ")
+valor2 = input ("Digite outro numero:")
+
+valor1 = int(valor1)
+valor2 = int(valor2)
+
+print(valor1 + valor2)"""
+
+valor1 = int(input("Digite um valor: "))
+valor2 = int(input("Digite outro valor: "))
+
+resultado = valor1 + valor2
+
+print(f"resultado: {resultado}")
