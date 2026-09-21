701bccccb9ea0e975f0a6af2a52c6f1c5bfbfd41
Kauan Cruz <kauancruzdasilva2@gmail.com>
2026-09-20 21:15:37 -0300

    Aula 6 - Tipos de dados primitivos

---
+++ Python basico/tipos_de_dados_primitivos.py
@@ -0,0 +1,31 @@
+"""1. Inteiro ( int )"""
+status_code = 200
+quantidade_usuarios = 15
+tentativas = 3
+print(status_code)
+print(quantidade_usuarios)
+print(tentativas)
+
+"""2. Ponto flutuante ( float )"""
+tempo_resposta = 1.42
+valor_total = 199.90
+print(tempo_resposta)
+print(valor_total)
+
+"""3-Texto (str)"""
+mensagem = "Usuário criado com sucesso"
+endpoint = "/api/login"
+nome_usuario = "admin"
+print(mensagem)
+print(endpoint)
+print(nome_usuario)
+
+"""4. Booleano ( bool )"""
+login_valido = True
+usuario_ativo = False
+print(login_valido)
+print(usuario_ativo)
+
+"""5. Valor nulo ( None )"""
+token = None
+print(token)
