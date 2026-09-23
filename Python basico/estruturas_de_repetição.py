bb5b0fc70b421878ab59e375dd1813b9d0745998
Kauan Cruz <kauancruzdasilva2@gmail.com>
2026-09-22 21:21:13 -0300

    Aula 6 - Tipos de dados primitivos

---
+++ Python basico/estruturas_de_repetição.py
@@ -0,0 +1,12 @@
+for numero in range(5):
+    print(numero)
+
+
+    """O numero 2 posterior ao 10 é para pular os numeros de 2 em 2"""
+for numero2 in range(0, 10, 2):
+    print(numero2)
+
+contador = 0
+while contador <7:
+    print(contador)
+    contador += 1
